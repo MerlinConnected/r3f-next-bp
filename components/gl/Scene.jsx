d5ff31a0e3eade3fff9b4002f6ce96be4850ead9
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-02-20 16:52:56 +0100

    feat(scene): model component

--- components/gl/Scene.jsx
+++ components/gl/Scene.jsx
@@ -6,6 +6,11 @@ import { useControls } from 'leva';
 import { Suspense, useRef, useState } from 'react';
 
 export default function Scene() {
+function Model() {
+  const meshRef = useRef();
+
+  const [hovered, hover] = useState(false);
+
   const { color } = useControls({
     color: {
       value: '#FF69B4',
@@ -13,6 +18,23 @@ export default function Scene() {
       format: (v) => v,
     },
   });
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
+  if (meshRef.current) console.log(meshRef.current);
+
+  return (
+    <mesh ref={meshRef} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} scale={hovered ? 2 : 1}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={color} roughness={0} />
+    </mesh>
+  );
+}
   return (
     <>
       <Canvas shadows>
@@ -32,6 +54,7 @@ export default function Scene() {
           <ambientLight intensity={1} />
           <Environment preset="city" />
         </Suspense>
+        <Model />
       </Canvas>
       <Loader />
     </>
